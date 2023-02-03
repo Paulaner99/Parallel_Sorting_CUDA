440b04236911851ff44706616b8197c27b1eb52e
Paulaner99 <rikipaolini99@gmail.com>
2023-02-03 18:26:59 +0100

    tested cuda

--- sort.cu
+++ sort.cu
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <time.h>
 
+#define N 20
+
 __global__ void cuda_hello(){
     printf("Hello World from GPU!\n");
 }
@@ -68,10 +70,74 @@ void shell_sort(int *a, int n, int p){
     */
 }
 
+__global__ void sort(int *a, int n){
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
 
+    if(idx < n){
+        for(int step=0; step < blockDim.x; step++){
+            if(step % 2 == 0 && idx % 2 == 0 && threadIdx.x < blockDim.x - 1){
+                if(a[idx] > a[idx+1]){
+                    int tmp = a[idx+1];
+                    a[idx+1] = a[idx];
+                    a[idx] = tmp;
+                }
+            }
+            if(step % 2 == 1 && idx % 2 == 1 && threadIdx.x < blockDim.x - 1){
+                if(a[idx] > a[idx+1]){
+                    int tmp = a[idx+1];
+                    a[idx+1] = a[idx];
+                    a[idx] = tmp;
+                }
+            }
+            __syncthreads(); 
+        }   
+    }
+
+
+}
+
+
+int main(){
+
+    int *a, *seq, *par;
+    int *d_a, *d_seq, *d_par;
+    const size_t size = N * sizeof(int);
+
+    /* Allocate space for device copies of a, b, c */
+    cudaMalloc((void **)&d_a, size);
+    cudaMalloc((void **)&d_seq, size);
+    cudaMalloc((void **)&d_par, size);
+    
+    /* Alloc space for host copies of a,b,c and setup input values */
+    a = (int *)malloc(size);
+    seq = (int *)malloc(size);
+    par = (int *)malloc(size);
+
+    // Init Array
+    for(int i=0; i < N; i++){
+        a[i] = rand() % 100;
+        printf("%d ", a[i]);
+    }
+    printf("\n\n");
+
+    /* Copy inputs to device */
+    cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
+
+    /* Launch sort() kernel on GPU */
+    sort<<<2,N/2>>>(d_a, N);
+
+    cudaMemcpy(a, d_a, size, cudaMemcpyDeviceToHost);
+
+    for(int i=0; i < N; i++){
+        printf("%d ", a[i]);
+    }
+
+}
+
+
+/*
 int main() {
     
-    const int N = 1000;
     clock_t start, stop;
     int a[N], b[N];
     bool good = true;
@@ -79,19 +145,9 @@ int main() {
     init_array(a, N);
     copy_array(a, b, N);
 
-    for(int i=0; i < N; i++){
-        printf("%d ", a[i]);
-    }
-    printf("\n");
-    for(int i=0; i < N; i++){
-        printf("%d ", b[i]);
-    }
-    printf("\n");
-
-
     selection_sort(a, N);
     start = clock();
-    shell_sort(b, N, 2);
+    shell_sort(b, N, 1);
     stop = clock();
     printf("%lf\n", (double)(stop-start) / CLOCKS_PER_SEC);
 
@@ -111,3 +167,4 @@ int main() {
     //cuda_hello<<<1,1>>>(); 
     return 0;
 }
+*/
