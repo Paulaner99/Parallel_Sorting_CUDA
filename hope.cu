1a0688e960e31414b5857a85227d644be7fe9379
Paulaner99 <rikipaolini99@gmail.com>
2023-03-22 11:39:10 +0100

    it magically works!

--- hope.cu
+++ hope.cu
@@ -2,9 +2,9 @@
 #include <math.h>
 #include <time.h>
 
-#define MAX_SHARED_ELEMS 8*1024
+#define MAX_SHARED_ELEMS 12*1024
 #define N 4*1024*1024
-#define MAX_LENGTH 64*1024*1024
+#define MAX_LENGTH 128*1024*1024
 
 
 void random_array(int *a, int n){
@@ -25,6 +25,15 @@ void copy_array(int *a, int *b, int n){
 }
 
 
+__global__ void copyArrayDevice(int *dst, int *src, int n, int blocks){
+    int threads = blocks * blockDim.x;
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    for(int i=idx; i < n; i+=threads){
+        dst[i] = src[i];
+    }
+}
+
+
 bool compare_arrays(int *a, int *b, int n){
     for(int i=0; i < n; i++){
         if(a[i] != b[i]){
@@ -35,7 +44,7 @@ bool compare_arrays(int *a, int *b, int n){
 }
 
 
-__device__ int binarySearchCount(int val, int *a, int first, int last, int ord){
+__host__ __device__ int binarySearchCount(int val, int *a, int first, int last, int ord){
     int left = first;
     int right = last;
     int mid;
@@ -84,7 +93,8 @@ __global__ void merge_in_mem(int *a, int n, int blocks){
         v[i] = a[i+off];
     }
 
-    for(int size=1; size < elems_block; size*=2){  
+    for(int size=1; size < elems_block; size*=2){
+        __syncthreads();
         for(int i=threadIdx.x; i < elems_block && i + off < n; i+=blockDim.x){
             int before = i%size + (i/(2*size)) * 2 * size;
             int ord = (i/size) % 2;
@@ -102,7 +112,6 @@ __global__ void merge_in_mem(int *a, int n, int blocks){
         for(int i=threadIdx.x; i < elems_block && i + off < n; i+=blockDim.x){
             v[i] = aux[i]; 
         }
-        __syncthreads();
     }
 
     for(int i=threadIdx.x; (i < elems_block) && (i + off < n); i+=blockDim.x){
@@ -123,12 +132,37 @@ __global__ void merge_out_mem(int *a, int *aux, int n, int size, int blocks){
         if(last > first){
             int idx = binarySearchCount(a[i], a, first, last, ord);
             aux[idx + before] = a[i];
-            //printf("\n%d -> %d\n", i, idx + before);
+            //printf("\n%d\t%d -> %d (%d + %d)\t%d %d (%d)\n", a[i], i, idx + before, idx, before, first, last, ord);
         } else {
             aux[before] = a[i];
-            //printf("\n%d -> %d\n", i, before);
+            //printf("\n%d\t%d -> %d\t%d %d (%d)\n", a[i], before, first, last, ord);
         }
     }
+}
+
+
+void mergeSort(int *a, int n){
+    int *aux = (int *)malloc(n * sizeof(int));
+    for(int size=1; size < n; size*=2){
+        for(int i=0; i < n; i++){
+            int before = i%size + (i/(2*size)) * 2 * size;
+            int ord = (i/size) % 2;
+            int first = (i/size + 1 - 2*ord) * size;
+            int last = min(first + size, n);
+            if(last > first){
+                int idx = binarySearchCount(a[i], a, first, last, ord);
+                aux[idx + before] = a[i];
+            } else {
+                aux[before] = a[i];
+            }
+        }
+        copy_array(aux, a, n);
+    }
+    printf("\n");
+	for(int i=0; i < n; i++){
+		//printf("%d ", a[i]);
+	}
+	printf("\n");
 }
 
 
@@ -150,12 +184,13 @@ void shellSortPar(int *a, int n){
     int threads = 1024;
     int elems_block = (n + blocks - 1) / blocks;
     
-    merge_in_mem<<<blocks, threads>>>(d_a, n, blocks);
+    //merge_in_mem<<<blocks, threads>>>(d_a, n, blocks);
     cudaDeviceSynchronize();
-    for(int size=elems_block; size < n; size*=2){
+    for(int size=1; size < n; size*=2){
         merge_out_mem<<<blocks, threads>>>(d_a, d_aux, n, size, blocks);
         cudaDeviceSynchronize();
         cudaMemcpy(d_a, d_aux, size, cudaMemcpyDeviceToDevice);
+        copyArrayDevice<<<blocks, threads>>>(d_a, d_aux, n, blocks);
         cudaDeviceSynchronize();
     }
    
@@ -165,7 +200,7 @@ void shellSortPar(int *a, int n){
 
 	printf("\n");
 	for(int i=0; i < n; i++){
-		printf("%d ", a[i]);
+		//printf("%d ", a[i]);
 	}
 	printf("\n");
 
@@ -179,7 +214,7 @@ bool test(int *a, int *b, int n){
     printf("\nnum elements: %d\n", n);
     /* SEQUENTIAL BITONIC SORT */
     start = clock();
-    //shellSortSeq(a, n, n/2);
+    //mergeSort(a, n);
     stop = clock();
     time_seq = double(stop-start+1) / double(CLOCKS_PER_SEC);
     thr_seq = double(n * (double(CLOCKS_PER_SEC) / double(stop-start+1)));
@@ -209,7 +244,7 @@ int main(){
 
     int *a, *b;
 
-    for(int length=4*1024-791; length < MAX_LENGTH; length*=2){
+    for(int length=MAX_LENGTH; length <= MAX_LENGTH; length*=2){
         // Allocate memory for the array
         a = (int *)malloc(length * sizeof(int));
         b = (int *)malloc(length * sizeof(int));
@@ -219,7 +254,7 @@ int main(){
         copy_array(a, b, length);
 
         test(a, b, length);
-		break;
+		//break;
         free(a); free(b);
     }
 }
